dc392573e6ff5bdc443ff4562611195b7b4e2825
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2022-07-04 17:21:23 +0530

    add application.properties and configure JPAConfig class

--- BackEnd/src/main/java/lk/easycarrent/app/config/JPAconfig.java
+++
@@ -1,9 +0,0 @@
-package lk.easycarrent.app.config;
-
-/**
- * @PROJECT EasyCarRental
- * @Author Rajith Sanjaya
- * @Date 2022 Jul 03
- **/
-public class JPAconfig {
-}
--- BackEnd/src/main/java/lk/easycarrent/app/config/WebRootConfig.java
+++ BackEnd/src/main/java/lk/easycarrent/app/config/WebRootConfig.java
@@ -10,6 +10,6 @@ import org.springframework.context.annotation.Import;
  **/
 
 @Configuration
-@Import(JPAconfig.class)
+@Import(JPAConfig.class)
 public class WebRootConfig {
 }
