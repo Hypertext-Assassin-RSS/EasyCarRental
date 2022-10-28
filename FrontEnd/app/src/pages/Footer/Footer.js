44127249f5e770dbd3cec1493a22128a501abc92
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-10-28 11:20:57 +0530

    footer compleate

--- FrontEnd/app/src/pages/Footer/Footer.js
+++ FrontEnd/app/src/pages/Footer/Footer.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import './Footer.css'
+import { Typography }  from '@material-ui/core'
+import '@fontsource/roboto/700.css';
+import { MdFacebook } from "react-icons/md";
+import { SiTwitter , SiGmail} from "react-icons/si";
+
 const Footer = () => {
   return (
-  <div className="container">
-    <h1 className="text-center" style={{paddingTop: "30%"}}>
-      Footer
-    </h1>
-
+  <div className="footer">
+    <div className='footer-container'>
+      <div className='title-container'>
+      <Typography variant="h4" gutterBottom>
+        Easy Car Rental
+      </Typography>
+      <span>Car Rental System</span>
+      </div>
+      <div className='footer-link-container'>
+        <div className='footer-link'>
+          <a  href='#'>Services</a>
+        </div>
+        <div className='footer-link'>
+          <a  href='#'>Contact</a>
+        </div>
+        <div className='social-icon-container'>
+          <div className='social-icon'> <a  href='#'><MdFacebook /></a> </div>
+          <div className='social-icon'> <a  href='#'><SiTwitter /></a></div>
+          <div className='social-icon'> <a  href='#'><SiGmail /></a></div>
+        </div>
+      </div>
+    </div>
+    <div className='copy-right'>
+      <span>Developed and maintained by © Rajith Sanjaya All right reserved</span>
+    </div>
   </div>
   )
 }
