eb6da36bd69fe47e5c11b7a191a37d2ae4d5e653
Hypertext-Assassin-RSS <sanjayasenanayaka11@gmail.com>
2022-10-28 15:54:07 +0530

    admin login page complete

--- FrontEnd/app/src/pages/Managment/Login/AdminLogin.js
+++ FrontEnd/app/src/pages/Managment/Login/AdminLogin.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import './AdminLogin.css'
+import InputUnstyled from '@mui/base/InputUnstyled';
+import ButtonUnstyled from '@mui/base/ButtonUnstyled';
+
 
 export default function AdminLogin() {
   return (
-    <div>AdminLogin</div>
+    <div className='admin-coantainer'>
+      <div className='input-container'>
+        <div className='input'><InputUnstyled /></div>
+        <div className='input'><InputUnstyled /></div>
+      </div>
+      <div className='button-container'> 
+      <ButtonUnstyled className='customer-btn'>
+        <a href='/Dash'>
+          Login
+        </a>
+        </ButtonUnstyled>
+      </div>
+      
+    </div>
   )
 }
